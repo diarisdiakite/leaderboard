50e37f240c6ceed15e3001c42e9a5af27599cec5
diarisdiakite <diarisdiakite@gmail.com>
2023-06-03 22:18:51 +0200

    Update webpack after adding axios

--- webpack.config.js
+++ webpack.config.js
@@ -31,4 +31,14 @@ module.exports = {
       },
     ],
   },
+  resolve: {
+    fallback: {
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      util: require.resolve('util'),
+      zlib: require.resolve('browserify-zlib'),
+      stream: require.resolve('stream-browserify'),
+      url: require.resolve('url'),
+    },
+  },
 };
