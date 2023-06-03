4a19b41b2842634e689c6f0c61a9283d6478abd0
diarisdiakite <diarisdiakite@gmail.com>
2023-06-03 22:28:28 +0200

    Add event listeners

--- src/index.js
+++ src/index.js
@@ -1,14 +1,42 @@
-import _ from 'lodash';
-import './style.css';
+import './style.css'
+import { displayScores } from './modules/eventFunctions/displayScores.js';
+import addEventFunction from './modules/eventFunctions/addScore.js';
+import footerText from './modules/globalElements/footer.js';
+import deleteScoresFunction from './modules/eventFunctions/removeScores.js';
+import updateDate from './modules/globalElements/date.js';
 
-function component() {
-  const element = document.createElement('div');
+//export { displayScores };
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+/* 
 
-  return element;
-}
+  Only EventListeners are listed here
 
-document.body.appendChild(component());
+
+*/
+
+const listSubtitle = document.getElementById('list-subtitle')
+const refreshButton = document.createElement('button');
+refreshButton.classList.add('button', 'add-button')
+refreshButton.textContent = 'Refresh';
+listSubtitle.appendChild(refreshButton);
+refreshButton.addEventListener('click', async (e) => {
+  e.preventDefault();
+  await displayScores()
+  //window.location.reload();
+})
+
+const addButton = document.querySelector('#addScore');
+const clearInputFields = () => {
+  document.getElementById('user').value = '';
+  document.getElementById('score').value = '';
+};
+
+addButton.addEventListener('click', async () => {
+  await addEventFunction();
+  clearInputFields();
+});
+
+
+// footer
+const myFooter = document.getElementById('footer-section');
+myFooter.innerHTML += footerText;
